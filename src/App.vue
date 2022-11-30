0245097fc1611f57677dc8acf6749911d5126e67
Ayush yadav <ayushyadav@gofynd.com>
2022-11-30 21:16:54 +0530

    header-commited

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,21 @@
 <script>
-
+import NavBar from './components/NavBar.vue';
+export default{
+  components : {
+  NavBar
+  }
+}
 </script>
 
 <template>
  <div class="content">
-  
+  <nav-bar/>
   <h1>Ayush Yadav</h1>
   <p>Work In Progress</p>
  </div>
 </template>
 
 <style scoped>
-.content {
-  color: rgb(74, 255, 216);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  height: 80vh;
-  align-items: center;
-  font-size: 3rem;
 
 
-}
-.content h1{
-  text-transform: uppercase;
-  animation: flow 1s infinite ease-in-out forwards;
-}
-.content p {
-  color: rgb(255, 7, 247);
-  /* border: 2px solid red; */
-  padding: 1rem;
-  border-radius: 10px;
-  animation: rotate 4s infinite linear;
-}
-@keyframes flow {
-  0%{
-    color: rgb(74, 255, 216);
-    transform: scale(105%);
-  }
-  100%{
-    color: rgb(69, 255, 122);
-    
-  }
-}
-@keyframes rotate {
-  0%{
-    border-top: 4px solid rgb(211, 34, 255);
-  }
-  25%{
-    border-right: 4px solid rgb(175, 255, 207);
-  }
-  50%{
-    border-bottom: 4px solid rgb(255, 238, 194);
-  }
-  75%{
-    border-left: 4px solid rgb(255, 108, 55);
-  }
- 100%{
-  border: 4px solid rgb(255, 255, 255);
- }
-
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+  name: "nav-bar",
+};
+</script>
+<template>
+  <div class="cont">
+    <div class="container">
+      <section class="links">
+        <a href="#">Work</a>
+      </section>
+      <section class="links">
+        <a href="#">About</a>
+      </section>
+      <section class="links">
+        <a href="#">Contact</a>
+      </section>
+    </div>
+
+    <div class="icons">
+      <section>
+        <a href="https://www.github.com/iamayushy" target="_blank">
+        <img
+          src="../assets/GitHub-Mark-Light-120px-plus.png"
+          alt="github-icon"
+          width="30px"
+        />
+    </a>
+      </section>
+      <section>
+        <a href="https://www.github.com/iamayushy" target="_blank">
+        <img
+        
+          src="../assets/linkedin.png"
+          alt="linkedin-icon"
+          width="30px"
+        />
+    </a>
+      </section>
+    </div>
+  </div>
+</template>
+<style scoped>
+.cont {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.5rem;
+}
+.container {
+  margin: 0 auto;
+  width: 90%;
+  display: flex;
+  gap: 4rem;
+  margin-left: 4rem;
+}
+.icons {
+  display: flex;
+  gap: 2rem;
+  margin-right: 4rem;
+}
+.links {
+  position: relative;
+}
+.links a {
+  color: white;
+  text-decoration: none;
+  font-size: 1.3rem;
+  cursor: pointer;
+}
+.links:hover::after {
+  position: absolute;
+  content: "";
+  background-color: white;
+  height: 2px;
+  border-radius: 8px;
+  bottom: -2%;
+  left: 0;
+  margin-top: 0.5rem;
+  animation: fill 0.3s linear forwards;
+}
+@keyframes fill {
+  0% {
+    width: 3%;
+  }
+  100% {
+    width: 100%;
+  }
+}
+</style>
